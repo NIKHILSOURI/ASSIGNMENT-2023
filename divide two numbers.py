6b6e1cf68ed74d809b8a277e1cf5d26448828d2c
NIKHIL SOURI <mymedia.yns@gmail.com>
2023-04-10 21:13:45 +0530

    Update divide two numbers.py

--- divide two numbers.py
+++ divide two numbers.py
@@ -1,5 +1,5 @@
 class Solution:
-    def divide(self, dividend: int, divisor: int) -> int:
+    def divide(self, dividend, divisor) :
         sign = -1 if (dividend >= 0 and divisor < 0) or (dividend < 0 and divisor >= 0) else 1
         dividend = abs(dividend)
         divisor = abs(divisor)
@@ -10,3 +10,5 @@ class Solution:
         plus_limit = (2**31 - 1)
         result = min(max(result, minus_limit), plus_limit)
         return result
+obj=Solution()
+print(obj.divide(,))
