8d668b769c40649a4eced83f21930b718381da71
NIKHIL SOURI <mymedia.yns@gmail.com>
2023-04-10 21:09:11 +0530

    Update REVERSE INTEGER.py

--- REVERSE INTEGER.py
+++ REVERSE INTEGER.py
@@ -1,9 +1,9 @@
 class Solution:
-    def reverse(self, f: int) -> int:
+    def reverse(self, f) :
         r = 0
         x = 0
         h = False
-        if 0>f:
+        if f<0:
             x = abs(f)
             h = True
         else:
@@ -12,10 +12,14 @@ class Solution:
             i = x%10
             r = r*10+i
             x//=10
-        if -2147483648 <= r <= 2147483647:
+        +if -2147483648 <=r  and r<= 2147483647:
             if h:
                 return -r
             else:
                 return r
         else:
             return 0
+
+
+obj=Solution()
+print(obj.reverse())
